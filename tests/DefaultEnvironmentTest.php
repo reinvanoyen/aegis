2e8bbb9d5c98777a51415f2b7c6c16ebfda65f55
Rein Van Oyen <reinvanoyen@gmail.com>
2016-11-02 15:10:36 +0100

    improved tests

---
+++ tests/DefaultEnvironmentTest.php
@@ -0,0 +1,36 @@
+<?php
+
+use \Aegis\Template;
+
+class DefaultEnvironmentTest extends PHPUnit_Framework_TestCase
+{
+	public function testBasics()
+	{
+		Template::$cacheDirectory = 'tests/cache/';
+		Template::$templateDirectory = 'tests/templates/';
+
+		$string = 'UOspIkq124MLd,shdfDRDFGbnjsjs,k,oao';
+		$number = 12;
+
+		$tpl = new Template();
+		$tpl->string = $string;
+		$tpl->number = $number;
+
+		$expectedResult = $string.' '.$number.' something';
+
+		$this->assertEquals($expectedResult, $tpl->render('basics-test') );
+	}
+
+	public function testIf()
+	{
+		Template::$cacheDirectory = 'tests/cache/';
+		Template::$templateDirectory = 'tests/templates/';
+
+		$tpl = new Template();
+		$tpl->condition = true;
+
+		$expectedResult = 'test 1test 2test 3test 4test 5test 6test 7test 8';
+
+		$this->assertEquals($expectedResult, $tpl->render('if-test') );
+	}
+}
--- tests/templates/if-test.tpl
+++ tests/templates/if-test.tpl
@@ -1,1 +1,8 @@
-{{ if @condition }}Condition is true{{ /if }}
+{{ if @condition }}test 1{{ /if }}
+{{ if not @condition or @condition }}test 2{{ /if }}
+{{ if @condition equals @condition }}test 3{{ /if }}
+{{ if @condition and @condition }}test 4{{ /if }}
+{{ if @condition and @condition and @condition }}test 5{{ /if }}
+{{ if @condition and true }}test 6{{ /if }}
+{{ if @condition and true }}{{ if true or @condition }}test 7{{ /if }}{{ /if }}
+{{ if not @condition }}{{ else }}test 8{{ /if }}
