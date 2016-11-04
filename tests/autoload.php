11418d281b8279e2ff1382adfc58f7ba01af8e5d
Rein Van Oyen <reinvanoyen@gmail.com>
2016-11-04 09:56:48 +0100

    improved autoloader

--- tests/autoload.php
+++ tests/autoload.php
@@ -1,17 +1,9 @@
 <?php
 
-include_once __DIR__ . '/../vendor/autoload.php';
-
-$classLoader = new \Composer\Autoload\ClassLoader();
-$classLoader->addPsr4('Aegis\\', __DIR__ . '/../lib/', true);
-$classLoader->register();
-
-/*
 function autoloader($classname)
 {
     $classname = ltrim($classname, '\\');
-    include_once 'lib/'.str_replace('\\', '/', $classname).'.php';
+    include_once __DIR__ . '/../lib/'.str_replace('\\', '/', $classname).'.php';
 }
 
 spl_autoload_register('autoloader');
-*/
